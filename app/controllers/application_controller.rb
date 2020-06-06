ab7981947144992af187a9d6a269de747a6c60b6
gitknkn <atio.asu@gmail.com>
2020-06-06 14:01:07 +0000

    add devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    case resource
+      when User
+        flash[:notice] = "#{resource.name}さんの投稿を楽しみにしています！"
+        root_path(resource)
+      when Admin
+        admins_top_path
+      end
+  end
+  
+  def after_sign_out_path_for(resource)
+    if resource != :admin
+      root_path
+    else
+      new_admin_session_path
+    end
+  end
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, #パスワードの正確性を検証
+        :registerable, #ユーザー登録や編集、削除
+        :recoverable, #パスワードをリセット
+        :rememberable, #ログイン情報を保存
+        :validatable #(emailのフォーマットなどのバリデーション)
+end
